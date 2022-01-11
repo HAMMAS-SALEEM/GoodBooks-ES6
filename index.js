6320c60b634606883ca1b282988486c18bb84ff0
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-11 21:39:56 +0500

    Fix Linters

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let book = 'name'
+import  pushListItem  from './modules/showbook.js';
+
+import  Collection  from './modules/addRemoveBtn.js';
+
+import { showListSection, showAddSection, showContactSection } from './modules/dom.js';
+
+import {DateTime} from './node_modules/luxon/build/es6/luxon.js';
+
+const book = 'name';
 const bookName = document.getElementById(book);
 const author = document.getElementById('author');
 const output = document.querySelector('.list-elements');
 const form = document.querySelector('#form');
-const removeBtn = document.querySelector('.removeButton')
 let bList = JSON.parse(localStorage.getItem('booksList'));
 const collection = new Collection(bList);
-import {
-  pushListItem
-} from "./modules/showbook.js"
-
-import {
-  Collection
-} from './modules/addRemoveBtn.js'
 
 pushListItem(bList, output);
 
@@ -21,7 +21,7 @@ const addBtn = document.querySelector('.addBtn');
 addBtn.addEventListener('click', () => {
   collection.addBooks(bookName, author);
   bList = JSON.parse(localStorage.getItem('booksList'));
-  pushListItem(bList, output)
+  pushListItem(bList, output);
   form.reset();
 });
 
@@ -35,36 +35,26 @@ const addSection = document.getElementById('add-book');
 const contactSection = document.getElementById('contact');
 const time = document.getElementById('time');
 
-import {
-  showListSection,
-  showAddSection,
-  showContactSection
-} from "./modules/dom.js"
-
-import {
-  DateTime
-} from './node_modules/luxon/build/es6/luxon.js';
-
 const timeNow = DateTime.now();
 time.textContent = timeNow.toLocaleString(DateTime.DATETIME_MED);
 
 navList.addEventListener('click', () => {
   showListSection(listSection, addSection, contactSection);
+  pushListItem(bList, output);
 });
 
 navAdd.addEventListener('click', () => {
-  showAddSection(listSection, addSection, contactSection)
+  showAddSection(listSection, addSection, contactSection);
 });
 
 navContact.addEventListener('click', () => {
-  showContactSection(listSection, addSection, contactSection)
+  showContactSection(listSection, addSection, contactSection);
 });
 
-output.addEventListener('click',(e)=>{
-  console.log(e)
+output.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
-    collection.removeBooks(e.target.id)
-    bList = JSON.parse(localStorage.getItem('booksList'))
-    pushListItem(bList, output)
+    collection.removeBooks(e.target.id);
+    bList = JSON.parse(localStorage.getItem('booksList'));
+    pushListItem(bList, output);
   }
 });
--- modules/showbook.js
+++ modules/showbook.js
@@ -1,12 +1,13 @@
-export function pushListItem(bookList, output) {
-  let containId;
+export default function pushListItem(bookList, output) {
   let bookHtml = '';
   const booksArray = bookList;
-  if (booksArray !== null) {
+  if (booksArray !== null || booksArray.lengnth !== 0) {
     booksArray.forEach((item) => {
       bookHtml += `<li class="list_item">
-    <p class="book-name">"${item.book}" by ${item.author}</p><br><button class ="remButton" id=${item.id} type="button">Remove</button></li>`;
-    });
-    output.innerHTML = bookHtml;
+      <p class="book-name">"${item.book}" by ${item.author}</p><br><button class ="remButton" id=${item.id} type="button">Remove</button></li>`;
+    })
+  } else {
+    bookHtml = '<li>No Book Avaiable</li>'
   }
+  output.innerHTML = bookHtml;
 }
